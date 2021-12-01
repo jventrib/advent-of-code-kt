0576dc72411f16ed3d8bbdb5d1eab2b99f5b804d
jventrib <jventrib@gmail.com>
2021-12-01 15:53:03 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.6.0"
+    java
 }
 
 repositories {
@@ -11,9 +12,20 @@ tasks {
         main {
             java.srcDirs("src")
         }
+        test {
+            java.srcDirs("test")
+        }
+    }
+    test {
+        useJUnitPlatform()
     }
 
     wrapper {
         gradleVersion = "7.3"
+    }
+
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     }
 }
