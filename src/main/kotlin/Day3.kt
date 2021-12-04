a00bb40278e69f59c508c36276ba43ca98a7dd6d
jventribout <jventribout@smartwavesa.com>
2021-12-04 10:20:01 +0100

    Day3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,39 +1,32 @@
 val day3 = day<Int>(3) {
     part1(198) {
-        val charList = this.flatMap { word ->
-            word.toCharArray().mapIndexed { i, c -> Pair(i, c.digitToInt(2)) }.toList()
-        }
-        val rates = charList
-            .groupBy { (pos, _) -> pos }
-            .map { (_, chars) ->
-                val countGroup = chars.map { it.second }.groupingBy { it }.eachCount()
-                countGroup.entries.maxByOrNull { it.value }!!.key to countGroup.entries.minByOrNull { it.value }!!.key
-            }
-            .fold(Pair<List<Int>, List<Int>>(listOf(), listOf())) { acc, e ->
-                acc.copy(acc.first + e.first, acc.second + e.second)
-            }
-        val gamma = rates.first.joinToString("").toInt(2)
-        val epsilon = rates.second.joinToString("").toInt(2)
+        val range = 0 until 5
+        val gamma = range.map { pos ->
+            getMostCommon(getCharsAtPos(pos))
+        }.joinToString("").toInt(2)
+
+        val epsilon = range.map { pos ->
+            getLeastCommon(getCharsAtPos(pos))
+        }.joinToString("").toInt(2)
         gamma * epsilon
     }
 
     part2(230) {
-        val oxygen = keepCommon(this, 0) { getMostCommon(it) }.toInt(2)
-        val co2 = keepCommon(this, 0) { getLeastCommon(it) }.toInt(2)
+        val oxygen = this.keepCommon(0) { getMostCommon(it) }.toInt(2)
+        val co2 = this.keepCommon(0) { getLeastCommon(it) }.toInt(2)
         oxygen * co2
     }
 }
 
-private fun keepCommon(list: List<String>, pos: Int, selector: (List<Int>) -> Int): String {
-    val charsAtPos = list.map { it[pos].digitToInt(2) }
-    val filtered = list.filter { it[pos].digitToInt(2) == selector(charsAtPos) }
-    if (filtered.size == 1) return filtered.first()
-    return keepCommon(filtered, pos + 1, selector)
+private fun List<String>.getCharsAtPos(pos: Int) = this.map { it[pos].digitToInt(2) }
+
+private fun List<String>.keepCommon(pos: Int, selector: (List<Int>) -> Int): String {
+    val filtered = filter { it[pos].digitToInt(2) == selector(getCharsAtPos(pos)) }
+    return if (filtered.size == 1) filtered.first() else filtered.keepCommon(pos + 1, selector)
 }
 
 private fun getMostCommon(charsAtPos: List<Int>) = getCommon(charsAtPos, 1, Map<Int, Int>::maxByOrNull)
 private fun getLeastCommon(charsAtPos: List<Int>) = getCommon(charsAtPos, 0, Map<Int, Int>::minByOrNull)
-
 
 private fun getCommon(
     list: List<Int>,
