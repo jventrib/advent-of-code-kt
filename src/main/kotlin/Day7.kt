00f2becc32fed95ec863cdcb498eb79d2bb2abcc
jventribout <jventribout@smartwavesa.com>
2021-12-07 18:48:48 +0100

    Day7 - Triangular numbers

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,14 +1,15 @@
+import kotlin.math.abs
 import kotlin.math.absoluteValue
 
 val day7 = day<Int>(7) {
     part1(expectedExampleOutput = 37, expectedOutput = 344535) {
         val crabs = parseLineToIntList()
-        (1..crabs.maxOf { it }).map { dest -> crabs.sumOf { (it - dest).absoluteValue } }.minOf { it }
+        (1..crabs.maxOf { it }).map { dest -> crabs.sumOf { abs(it - dest) } }.minOf { it }
     }
 
     part2(expectedExampleOutput = 168, expectedOutput = 95581659) {
         val crabs = parseLineToIntList()
-        (1..crabs.maxOf { it }).map { dest -> crabs.sumOf { (1..(dest - it).absoluteValue).sum() } }.minOf { it }
+        (1..crabs.maxOf { it }).map { dest -> crabs.sumOf { abs(dest - it).let { i -> (i * i + i) / 2 } } }.minOf { it }
     }
 }
 
