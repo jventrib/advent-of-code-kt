bd026dfdbbab8e358696a1325cee484950c08f40
jventribout <jventribout@smartwavesa.com>
2021-12-13 09:17:42 +0100

    Day12 ok

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -2,72 +2,55 @@ typealias Path = List<Cave>
 
 val day12 = day<Int>(12) {
     part1(expectedExampleOutput = 10, expectedOutput = 3463) {
-        val connections = this.map {
-            it.split("-")
-        }.flatMap { cave ->
-            if (cave.none { it == "start" || it == "end" }) listOf(
-                Cave(cave.first()) to Cave(cave.last()),
-                Cave(cave.last()) to Cave(cave.first())
-            )
-            else {
-                when {
-                    cave.last() == "start" -> listOf(Cave(cave.last()) to Cave(cave.first()))
-                    cave.first() == "end" -> listOf(Cave(cave.last()) to Cave(cave.first()))
-                    else -> listOf(Cave(cave.first()) to Cave(cave.last()))
-                }
-            }
-        }
+        val group: Map<Cave, List<Cave>> = getConnections()
 
-
-        val group: Map<Cave, List<Cave>> = connections.groupBy { it.first }.mapValues { it.value.map { it.second } }
-
-        val paths = buildPath(group, listOf(listOf(Cave("start"))), true).filter { it.contains(Cave("end")) }
-//        println("paths: $paths")
+        val paths = buildPath(group, listOf(Cave("start")), true).filter { it.contains(Cave("end")) }
         paths.size
     }
 
-    part2(expectedExampleOutput = 36, expectedOutput = 0) {
-        val connections = this.map {
-            it.split("-")
-        }.flatMap { cave ->
-            if (cave.none { it == "start" || it == "end" }) listOf(
-                Cave(cave.first()) to Cave(cave.last()),
-                Cave(cave.last()) to Cave(cave.first())
-            )
-            else {
-                when {
-                    cave.last() == "start" -> listOf(Cave(cave.last()) to Cave(cave.first()))
-                    cave.first() == "end" -> listOf(Cave(cave.last()) to Cave(cave.first()))
-                    else -> listOf(Cave(cave.first()) to Cave(cave.last()))
-                }
-            }
-        }
-        val group: Map<Cave, List<Cave>> = connections.groupBy { it.first }.mapValues { it.value.map { it.second } }
-
-        val paths = buildPath(group, listOf(listOf(Cave("start"))), false).filter { it.contains(Cave("end")) }
-//        println("paths: $paths")
+    part2(expectedExampleOutput = 36, expectedOutput = 91533) {
+        val group: Map<Cave, List<Cave>> = getConnections()
+        val paths = buildPath(group, listOf(Cave("start")), false).filter { it.contains(Cave("end")) }
         paths.size
     }
 }
 
-
-fun buildPath(group: Map<Cave, List<Cave>>, paths: List<Path>, onceForSmall: Boolean): List<Path> {
-    val newPaths = paths.asSequence().flatMap { path ->
-        val targets = getTargets(group, path, onceForSmall)
-        targets?.map { path + it } ?: paths
-    }
-        .filter { path ->
-            val eachCount = path.filter { it.isLowerCase() }.groupingBy { it }.eachCount()
-            eachCount.values.reduce { acc, entry -> acc * entry } <= 2
+private fun List<String>.getConnections(): Map<Cave, List<Cave>> {
+    val connections = this.map {
+        it.split("-")
+    }.flatMap { cave ->
+        if (cave.none { it == "start" || it == "end" }) listOf(
+            Cave(cave.first()) to Cave(cave.last()),
+            Cave(cave.last()) to Cave(cave.first())
+        )
+        else {
+            when {
+                cave.last() == "start" -> listOf(Cave(cave.last()) to Cave(cave.first()))
+                cave.first() == "end" -> listOf(Cave(cave.last()) to Cave(cave.first()))
+                else -> listOf(Cave(cave.first()) to Cave(cave.last()))
+            }
         }
-        .distinct().toList()
-    println(paths.size)
-    if (newPaths == paths) return paths
-    return buildPath(group, newPaths, onceForSmall)
+    }
+
+    return connections.groupBy { it.first }.mapValues { it.value.map { it.second } }
 }
 
-private fun getTargets(group: Map<Cave, List<Cave>>, path: Path, onceForSmall: Boolean = true) =
-    group[path.last()]?.filterNot { onceForSmall && it.isLowerCase() && path.contains(it) }
+
+fun buildPath(group: Map<Cave, List<Cave>>, path: Path, onceForSmall: Boolean): List<Path> {
+    if (path.last().s == "end") return listOf(path)
+    val newPaths = getTargets(group, path, onceForSmall).map { path + it }
+        .filter { p ->
+            p
+                .filter { it.isLowerCase() }
+                .groupingBy { it }
+                .eachCount().values
+                .reduce { acc, entry -> acc * entry } <= 2
+        }
+    return newPaths.flatMap { buildPath(group, it, onceForSmall) }
+}
+
+private fun getTargets(group: Map<Cave, List<Cave>>, path: Path, onceForSmall: Boolean = true): Path =
+    group.getValue(path.last()).filterNot { onceForSmall && it.isLowerCase() && path.contains(it) }
 
 private fun Cave.isLowerCase() = s.lowercase() == s
 
