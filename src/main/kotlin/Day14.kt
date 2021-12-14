39cf84059da0810bdb0e8554f1dd0d0cdd5c617b
jventribout <jventribout@smartwavesa.com>
2021-12-14 18:56:11 +0100

    Day14 Part2 WIP

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,26 +1,37 @@
+import kotlin.math.ceil
+
 val day14 = day<Long>(14) {
     part1(expectedExampleOutput = 1588, expectedOutput = 3306) {
         val polymer = first()
         val rules = getRules()
 
+        val initialCounts = rules.map { Rule(it.first, it.second.first(), polymer.count(it.first)) }
 
-        val result = (1..10).fold(polymer) { acc, i ->
-            doStep(rules, acc).also { println("after step $i :$it") }
+        val result = (1..10).fold(initialCounts) { acc, i ->
+            doStep(acc).also { println("after step $i :$it") }
         }
 
-        getResult(result)
+        val charCounts =
+            result.map { it.input to it.count }.flatMap { r -> r.first.toCharArray().map { it to r.second } }
+        val totals = charCounts.groupBy { it.first }.mapValues { ceil(it.value.sumOf { it.second }/2.0).toLong() }
+        totals.values.maxOf { it } - totals.values.minOf { it }
+
     }
 
     part2(expectedExampleOutput = 2188189693529, expectedOutput = 0) {
         val polymer = first()
         val rules = getRules()
 
+        val initialCounts = rules.map { Rule(it.first, it.second.first(), polymer.count(it.first)) }
 
-        val result = (1..40).fold(polymer) { acc, i ->
-            doStep(rules, acc).also { println("after step $i") }
+        val result = (1..40).fold(initialCounts) { acc, i ->
+            doStep(acc).also { println("after step $i :$it") }
         }
 
-        getResult(result)
+        val charCounts =
+            result.map { it.input to it.count }.flatMap { r -> r.first.toCharArray().map { it to r.second } }
+        val totals = charCounts.groupBy { it.first }.mapValues { ceil(it.value.sumOf { it.second }/2.0).toLong() }
+        totals.values.maxOf { it } - totals.values.minOf { it }
     }
 }
 
@@ -34,16 +45,31 @@ private fun List<String>.getRules() = this.drop(2).map { line ->
 }
 
 
-private fun doStep(rules: List<Pair<String, String>>, polymer: String): String {
+private fun doStep(rules: List<Rule>): List<Rule> {
+    val newRules = rules
+        .filter { it.count > 0 }
+        .flatMap { r -> (0 until r.count).flatMap { r.insert().windowed(2) } }
+        .map { o -> rules.getRule(o) }
+        .groupingBy { it.input }.eachCount()
 
-    return rules.fold(polymer) { acc, e ->
-        var tmp = acc
-        while (tmp.contains(e.first)) {
-            tmp = tmp.substringBefore(e.first) + e.insert() + tmp.substringAfter(e.first)
-        }
-        tmp
-    }.uppercase()
+    return rules.map { r -> r.copy(count = newRules[r.input] ?: 0) }
+
+//    val newRules = rules.flatMap {
+//        val windowed = it.insert().windowed(2)
+//        val newChars = windowed.map { r ->
+//            val rule = rules.firstOrNull { it.input == r }
+//            rule?.copy(count = rule.count + 1)
+//        }.filterNotNull()
+//        println(newChars)
+//        newChars
+//    }.distinct()
 
 }
 
-private fun Pair<String, String>.insert() = first.first() + second.lowercase() + first.last()
+private fun List<Rule>.getRule(input: String) = first { it.input == input }
+
+private fun Rule.insert() = "${input.first()}$output${input.last()}"
+
+private data class Rule(val input: String, val output: Char, val count: Int)
+
+private fun String.count(s: String) = this.windowed(s.length).count { it == s }
