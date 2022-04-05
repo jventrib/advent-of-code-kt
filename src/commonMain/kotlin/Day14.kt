ef46a008279abdc512d91ce4a1491a351d3aabc9
jventrib <jventrib@gmail.com>
2022-04-05 09:45:15 +0200

    Update Day14.kt

--- src/commonMain/kotlin/Day14.kt
+++ src/commonMain/kotlin/Day14.kt
@@ -13,27 +13,26 @@ val day14 = day<Long>(14) {
 private fun List<String>.doPart(step: Int): Long {
     val polymer = first()
     val rules = drop(2)
-        .map { line -> line.split(" -> ").let { it.first() to it.last() } }
-        .map { Rule(it.first, it.second.first(), polymer.count(it.first).toLong()) }
+        .map { line -> line.split(" -> ") }
+        .map { Rule(it.first(), it.last().first(), polymer.count(it.first()).toLong()) }
 
     val finalRules = (1..step).fold(rules) { acc, i ->
         doStep(acc).also { println("after step $i :$it") }
     }
 
     val totals = finalRules
-        .map { it.input to it.count }
-        .flatMap { r -> r.first.toCharArray().map { it to r.second } }
+        .flatMap { r -> r.input.map { it to r.count } } //get elements counts
         .groupBy { it.first }
         .mapValues { e -> ceil(e.value.sumOf { it.second } / 2.0).toLong() }
     return totals.values.maxOf { it } - totals.values.minOf { it }
 }
 
 private fun doStep(rules: List<Rule>): List<Rule> {
-    val newRules = rules
+    val counts = rules
         .filter { it.count > 0 }
-        .flatMap { r -> r.insert().windowed(2).map { it to r.count } }
-        .groupBy { it.first }.mapValues { e -> e.value.sumOf { it.second } }
-    return rules.map { r -> r.copy(count = newRules[r.input] ?: 0L) }
+        .flatMap { r -> r.insert().windowed(2).map { it to r.count } } //get count of new pairs created after insertion
+        .groupBy { it.first }.mapValues { e -> e.value.sumOf { it.second } } //suppress duplicates
+    return rules.map { r -> r.copy(count = counts[r.input] ?: 0L) } //update rules with new counts
 }
 
 private fun String.count(s: String) = this.windowed(s.length).count { it == s }
